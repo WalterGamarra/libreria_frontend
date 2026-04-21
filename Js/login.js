50a6c5d0f4f16bd6945cae5a8f0393883d84a1f9
Walter Gamarra <waltergamarra572@gmail.com>
2026-04-21 11:09:00 -0300

    fix:rutas backend

--- Js/login.js
+++ Js/login.js
@@ -1,6 +1,6 @@
 // login.js
 
-const API_AUTH = 'http://libreria-production-8cc8.up.railway.app';
+const API_AUTH = 'https://libreria-production-8cc8.up.railway.app';
 
 // Si ya hay token válido, saltar directo al panel
 if (getToken()) {
